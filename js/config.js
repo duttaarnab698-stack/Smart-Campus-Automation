8b8e67a502d391c33eaa6811210a9adba245cd75
duttaarnab698-stack <duttaarnab698@gmail.com>
2026-08-23 16:39:19 +0530

    Connect frontend to production backend

--- js/config.js
+++ js/config.js
@@ -1,2 +1,3 @@
 // Set the deployed Render API URL here after backend deployment.
-window.SMART_CAMPUS_API_URL = "http://localhost:5000";
+window.SMART_CAMPUS_API_URL =
+  "https://smart-campus-automation.onrender.com";
